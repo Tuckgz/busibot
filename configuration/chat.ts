48b19b9cd4fc52ea782f1a7b83bf3e94bb160ec1
Tucker Green-Zhu <tuckeragreen@gmail.com>
2025-03-03 17:31:44 -0500

    changing up the theme

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,6 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Yo, I'm ${AI_NAME}, ${OWNER_NAME}'s AI work buddy.`;
+export const INITIAL_MESSAGE: string = `Yo, I'm ${AI_NAME}, ${OWNER_NAME}'s AI Legal code sifter. Ask away!`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Oof, I feel dizzy. Can you give me a sec? I need some water before I answer more questions.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
